e8334b5566dda05050a24eab70226cd788ad46fc
Jorge <jhernan1337@gmail.com>
2021-02-23 21:06:34 -0700

    Added scripts

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');  
+const express = require('express');
 const APIroutes = require('./routes');
 const sequelize = require('./config/connection');
-const { User } = require("./models");
+const { User } = require('./models');
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
+const cors = require('cors');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,18 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-require("./config/passport")( User );
+require('./config/passport')(User);
 
-app.use(APIroutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
-// Send every request to the React app
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(APIroutes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
